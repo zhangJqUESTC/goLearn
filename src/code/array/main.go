91bc0d3be50d1999035f8e6a05e5c674249d2bbe
zhangjianqiu7 <zhangjianqiu7@jd.com>
2024-07-08 22:50:15 +0800

    slice

--- src/code/array/main.go
+++ src/code/array/main.go
@@ -4,9 +4,12 @@ package main
 import "fmt"
 
 func main() {
-	var a [3]int //数组的类型由元素类型与长度构成, 数组的数量一旦确定就不能更改了，就是说不可变
+	var a [3]int //数组的类型由元素类型与长度构成, 数组的数量一旦确定就不能更改了，就是说大小不可变
 	var b [4]int
 	fmt.Printf("value: %d\n", a[0])
 	fmt.Printf("value: %d\n", b[0])
 	fmt.Println(a)
+	// 定义
+	// 遍历
+	// 二维数组
 }
---
+++ src/code/slice/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// make函数构造切片
+	var a []string = make([]string, 5)
+	var b []int     //此时的b是nil
+	var e = []int{} //声明且初始化, 已经不是nil了，所以不能通过一个切片是否是nil的来判断是否是空的，需要通过len()
+	a[0] = "sichuan"
+	// b[0] = 1
+	var c = [6]int{1, 2, 3, 4, 5, 6} //为啥数组的显示类型声明是这样
+	b = c[1:5]
+	// 基于数组得到切片
+	fmt.Println(b)
+	// 切片的底层实现原理
+	// 切片不能直接比较
+	// 切片的赋值拷贝
+	// 切片的遍历
+	// 切片的扩容
+	var d string = "abcd"
+	fmt.Printf(d)
+	fmt.Println(e)
+}
