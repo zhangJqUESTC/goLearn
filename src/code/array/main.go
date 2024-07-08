f97030782440e10cc358e51b5a2c8ef5a99045c1
zhangjianqiu7 <zhangjianqiu7@jd.com>
2024-07-08 21:46:28 +0800

    main

---
+++ src/code/array/main.go
@@ -0,0 +1,12 @@
+package main
+
+// 数组相关内容
+import "fmt"
+
+func main() {
+	var a [3]int //数组的类型由元素类型与长度构成, 数组的数量一旦确定就不能更改了，就是说不可变
+	var b [4]int
+	fmt.Printf("value: %d\n", a[0])
+	fmt.Printf("value: %d\n", b[0])
+	fmt.Println(a)
+}
